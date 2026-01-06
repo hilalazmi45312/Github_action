192416557706edf33e8af7369d7e4de996b3dab9
iliascloone <ilias@cloone.com.my>
2026-01-07 07:54:09 +0800

    Insider : fix-search-url

--- plugins/senheng_core/app/Controllers/InsiderController.php
+++ plugins/senheng_core/app/Controllers/InsiderController.php
@@ -986,31 +986,26 @@ add_action('wp_ajax_nopriv_woodmart_remove_from_wishlist', 'track_multiple_wishl
 function track_search_term()
 {
     if (is_search()) {
-        $search_term = get_search_query();
-        $search_url  = home_url(add_query_arg(null, null));
-
-        echo "
+        $search_data = [
+            'type'  => 'custom_event',
+            'value' => [[
+                'event_name'       => 'searched',
+                'event_parameters' => [
+                    'keyword'      => get_search_query(),
+                    'searched_url' => home_url(add_query_arg(null, null)),
+                ],
+            ]],
+        ];
+        ?>
         <script>
             window.InsiderQueue = window.InsiderQueue || [];
-            let searchData = {
-                type: 'custom_event',
-                value: [{
-                    event_name: 'searched',
-                    event_parameters: {
-                        keyword: '" . esc_js($search_term) . "',
-                        searched_url: '" . esc_js($search_url) . "'
-                    }
-                }]
-            };
-
+            const searchData = <?php echo wp_json_encode($search_data); ?>;
             window.InsiderQueue.push(searchData);
-            window.InsiderQueue.push({
-                type: 'init'
-            });
+            window.InsiderQueue.push({ type: 'init' });
 
             console.log('📡 Search Event Fired:', searchData);
         </script>
-        ";
+        <?php
     }
 }
 add_action('wp_footer', 'track_search_term');
