f6b16dbaacf30b3e276383d01429bfd4a64ddec6
hilalazmi45312 <hilalazmi@cloone.com.my>
2026-01-07 15:06:50 +0800

    Enhance SEO export/convert

--- plugins/yoast-meta-import-export/build/index.asset.php
+++ plugins/yoast-meta-import-export/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a5c0d7ed2f100b23eb10');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '672f738645208472b098');
