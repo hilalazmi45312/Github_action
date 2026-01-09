c2d58e5eb57b343e3e4cb494c597b3b038d4bf1d
hilalazmi45312 <hilalazmi@cloone.com.my>
2026-01-09 16:24:13 +0800

    Display error on each row got issue during I/E Yoast SEO

--- plugins/yoast-meta-import-export/build/index.asset.php
+++ plugins/yoast-meta-import-export/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2fb4b98708cb9152bbe4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6ff03e10b8bda06c984f');
